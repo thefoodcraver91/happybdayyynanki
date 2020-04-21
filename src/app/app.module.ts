bfbcdf65aed64eece79188e7edd18d9201a667ea
tony stark <tony@starkindustries>
2020-04-21 17:29:02 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
+ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { VideoComponent } from './video/video.component';
+import { AudioComponent } from './audio/audio.component';
+import { MessageComponent } from './message/message.component';
+import {RouterModule, Routes} from '@angular/router';
+
+const routes:Routes = [
+{path:'',redirectTo:'users',pathMatch:'full'},  
+{path:'message',component:MessageComponent},
+{path:'video',component:VideoComponent},
+{path:'audio',component:AudioComponent},
+{path:'**',component:MessageComponent}
+
+
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VideoComponent,
+    AudioComponent,
+    MessageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
